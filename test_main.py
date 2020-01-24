a0412ede92b752adeccd08b509673fbc54d4c97d
Lullu08 <lu-braeuer@web.de>
2020-01-24 01:22:37 +0100

    bad test

--- test_main.py
+++ test_main.py
@@ -24,8 +24,8 @@ def client():
 
 def test_health(client):
     response = client.get('/')
-    assert response.status_code == 200
-    assert response.json == 'Healthy'
+    assert response.status_code == 500
+    assert response.json == 'Healthyyyhtrdht'
 
 
 def test_auth(client):
